aa943b9f730faa327e87f57159828678a787b8db
JPauloMorais <jpgmorais3@gmail.com>
2015-02-23 00:23:14 -0300

    Menu-splash

--- RedSparrowGLES20/src/br/jp/redsparrow/game/activities/MenuActivity.java
+++ RedSparrowGLES20/src/br/jp/redsparrow/game/activities/MenuActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.ConfigurationInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -17,6 +18,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 import android.widget.TextView;
@@ -28,6 +30,7 @@ import br.jp.redsparrow.game.ReSpMenuRenderer;
 public class MenuActivity extends Activity implements OnTouchListener {
 
 	private GameView mView;
+	private AnimationDrawable mSplashAnim;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,20 +65,15 @@ public class MenuActivity extends Activity implements OnTouchListener {
 		
 		relLayout.addView(mView);
 		
+		ImageView mSplash = new ImageView(this);
 		RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);	
+		layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+		mSplash.setLayoutParams(layoutParams);
+		mSplash.setBackgroundResource(R.drawable.intro_splash);
+		mSplashAnim = (AnimationDrawable) mSplash.getBackground();
 		
-		TextView tv = new TextView(this);
-		Typeface font = Typeface.createFromAsset(getAssets(), "fonts/Sertig.otf");
-		tv.setTypeface(font);
-		tv.setText("RED SPARROW");
-		tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-		tv.setTextSize(30);
-		
-		tv.setTextColor(Color.RED);
-		tv.setLayoutParams(layoutParams);		
-		relLayout.addView(tv);
-		
+		relLayout.addView(mSplash);
+				
 		RelativeLayout.LayoutParams layoutParams1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		layoutParams1.addRule(RelativeLayout.CENTER_IN_PARENT);
 		ImageButton playB = new ImageButton(this);
@@ -88,12 +86,18 @@ public class MenuActivity extends Activity implements OnTouchListener {
 			@Override
 			public void onClick(View v) {
 				startActivity(intent);
-				onStop();
 			}
 		});
 		relLayout.addView(playB);
 		
 		setContentView(relLayout);
+	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		if(hasFocus) {
+			mSplashAnim.start();
+		}
 	}
 
 	@Override
